d1d4b05a195a64b802fbbcbbfafa77f8f1546e48
dev-derisan <dev.derisan@gmail.com>
2024-08-28 07:11:41 +0900

    implement selection sort

---
+++ SelectionSort/SelectionSort.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+void SelectionSort(std::vector<int>& nums)
+{
+  for (size_t i = 0; i < nums.size(); ++i)
+  {
+    auto startIter = (std::begin(nums) + i);
+    auto minIter = std::min_element(startIter, std::end(nums));
+    std::iter_swap(startIter, minIter);
+  }
+}
+
+void SelectionSort2(std::vector<int>& nums)
+{
+  for (size_t i = 0; i < nums.size(); ++i)
+  {
+    size_t minIndex = i;
+    for (size_t j = i; j < nums.size(); ++j)
+    {
+      if (nums[minIndex] > nums[j])
+      {
+        minIndex = j;
+      }
+    }
+    std::swap(nums[i], nums[minIndex]);
+  }
+}
+
+
+int main()
+{
+  std::vector<int> nums{ 1, 5, -1, 8, 10, 9, 6 };
+  SelectionSort2(nums);
+
+  for (const int num : nums)
+  {
+    std::cout << num << std::endl;
+  }
+}
