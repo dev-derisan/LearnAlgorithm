ede80cd4fbe2459da374e6d8cb6afd3c33e5d122
dev-derisan <dev.derisan@gmail.com>
2024-10-03 21:21:58 +0900

    optimizing p1316

--- Implementation/1316.cpp
+++ Implementation/1316.cpp
@@ -5,14 +5,21 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <array>
 
 bool IsGroupWord(const std::string& word) {
   std::string temp = word;
   temp.erase(std::unique(temp.begin(), temp.end()), temp.end());
 
-  return std::all_of(temp.begin(), temp.end(), [&temp](const char ch) {
-    return (std::count(temp.begin(), temp.end(), ch) == 1);
-  });
+  std::array<int, 26> seen = {};
+  for (const char ch : temp) {
+    ++seen[ch - 'a'];
+    if (seen[ch - 'a'] > 1) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 void P1316() {
