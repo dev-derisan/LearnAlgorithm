5f7207ca1c90e08635214b57a344d3fa5f056e86
dev-derisan <dev.derisan@gmail.com>
2024-10-07 06:54:08 +0900

    solve p2743

---
+++ Implementation/2743.cpp
@@ -0,0 +1,18 @@
+// https://www.acmicpc.net/problem/2743
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+void P2743() {
+  std::cin.tie(NULL);
+  std::ios::sync_with_stdio(false);
+
+  std::string str;
+  std::cin >> str;
+
+  std::cout << str.size();
+}
