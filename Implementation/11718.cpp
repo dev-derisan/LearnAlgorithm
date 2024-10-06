aa89a458ba964dc9e87308bc44f59fef8f0a2452
dev-derisan <dev.derisan@gmail.com>
2024-10-06 22:10:17 +0900

    solve p11718

---
+++ Implementation/11718.cpp
@@ -0,0 +1,19 @@
+// https://www.acmicpc.net/problem/11718
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+int main() {
+  std::cin.tie(NULL);
+  std::ios::sync_with_stdio(false);
+
+  std::string str;
+
+  while (std::getline(std::cin, str)) {
+    std::cout << str << std::endl;
+  }
+}
